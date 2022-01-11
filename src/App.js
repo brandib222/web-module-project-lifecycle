83b76c42601d9706f36ed8143cc9e6457715f151
brandib222 <brandi.danielle.ball@gmail.com>
2022-01-11 17:09:33 -0500

    working on the followers now

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ class App extends React.Component {
         <input placeholder='github handle'/>
         <button>Search</button>
       </form>
+
+      <div className='user-info'>
+        <img src='https://avatars.githubusercontent.com/u/91443920?v=4' />
+        <a target='_blank' href='https://github.com/brandib222'> <h3>Brandi Ball</h3> </a>
+        <p>(brandib222)</p>
+        <p>Total Repos: 41</p>
+        <p>Total Followers: 6</p>
+      </div>
+      <div className='followers'>
+        <div className='follower'>
+          <img src='https://avatars.githubusercontent.com/u/55255486?v=4' />
+          <p>Marcel-rodriguez</p>
+        </div>
+      </div>
+
+
     </div>);
   }
 }
